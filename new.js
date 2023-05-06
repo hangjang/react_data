9275e401e4e3a07e9df9313c841930c6b94888be
hangjang <frontweb99@gmail.com>
2023-05-07 00:35:01 +0900

    230507

---
+++ new.js
@@ -0,0 +1,36 @@
+let data=[
+    {
+        id:0,
+        imgUrl:"img/new1.jpg",
+        title:"★★하드 톡SET 클로버베어",
+        price:"30,000원",
+        discount:"14,800원",
+        percent:"51%"
+    },
+    {
+        id:1,
+        imgUrl:"img/new2.jpg",
+        title:"[투명]★★아크릴세트 럽미럽미(블루)+P-Molli",
+        price:"33,000원",
+        discount:"16,200원",
+        percent:"51%"
+    },
+    {
+        id:2,
+        imgUrl:"img/new3.jpg",
+        title:"★★하드 톡SET P-Molli",
+        price:"33,000원",
+        discount:"14,800원",
+        percent:"51%"
+    },
+    {
+        id:3,
+        imgUrl:"img/new4.jpg",
+        title:"P-Lolli★카드수납",
+        price:"22,000원",
+        discount:"11,800원",
+        percent:"46%"
+    }
+]
+
+export default data;
---
+++ new1.js
@@ -0,0 +1,36 @@
+let data=[
+    {
+        id:12,
+        imgUrl:"img/new13.png",
+        title:"(카드탱크) 스마일꽃",
+        price:"25,000원",
+        discount:"14,800원",
+        percent:"41%"
+    },
+    {
+        id:13,
+        imgUrl:"img/new14.gif",
+        title:"(젤리) 댑시리즈",
+        price:"18,000원",
+        discount:"10,800원",
+        percent:"40%"
+    },
+    {
+        id:14,
+        imgUrl:"img/new15.jpg",
+        title:"(카드탱크) 볼따구 HB 버니",
+        price:"25,000원",
+        discount:"14,800원",
+        percent:"41%"
+    },
+    {
+        id:15,
+        imgUrl:"img/new16.jpg",
+        title:"(카드탱크) HB 버니 리본 빼꼼",
+        price:"25,000원",
+        discount:"14,800원",
+        percent:"41%"
+    }
+]
+
+export default data;
